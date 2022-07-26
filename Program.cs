0d6e07266a07339388f2e6f8486a9bde50f55c17
david <davidb31@gmail.com>
2022-07-26 22:25:01 +0300

    try to pick item options from a dropdown list

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,15 +22,35 @@ namespace ConsoleApp6
 
 			IWebDriver ChromeDriver  = new ChromeDriver();
 			ChromeDriver.Manage().Window.Maximize();
-			ChromeDriver.Navigate().GoToUrl("https://www.yad2.co.il/");
+			//ChromeDriver.Navigate().GoToUrl("https://www.yad2.co.il/");
 			ChromeDriver.Navigate().GoToUrl("https://www.yad2.co.il/vehicles/cars");
 			NavigateToadvanceSearch(ChromeDriver);
+			PickItemByCategory(ChromeDriver, "אאודי", "יצרן") ;
 		}
 		public static void NavigateToadvanceSearch(IWebDriver ChromeDriver)
 		{
 			var adavanceSearchCssSelector = "#__layout > div > main > div > div.main_body > div.LD_search_wrapper > div > div > form > ul > li.search_column.col_x3.advanced_search.advance_search_tooltip > div > div.dropdown_btn > div > button";
 			IWebElement element = ChromeDriver.FindElement(By.CssSelector(adavanceSearchCssSelector));
 			element.Click();
+
+		}
+		public static void PickItemByCategory(IWebDriver ChromeDriver, string itemName,string categoryName)
+		{
+			var categoryXpath = "//*[@id='__layout']/div/main/div/div[4]/div[4]/div/div/form/ul/li[1]/div/label/select";
+			var itemListSelector = ChromeDriver.FindElement(By.XPath(categoryXpath));
+			//object of SelectElement
+			SelectElement s = new SelectElement(itemListSelector);
+			//Options method to get all options
+			s.SelectByText(itemName);
+			var result = s.SelectedOption;
+			var result1 = result;
+
+		}
+		public static By SelectorByAttributeValue(string p_strAttributeName, string p_strAttributeValue)
+		{
+			return (By.XPath(String.Format("//*[@{0} = '{1}']",
+										   p_strAttributeName,
+										   p_strAttributeValue)));
 		}
 	}
 
